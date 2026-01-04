9c765f48c7543db172eed29122bc902e70e2f196
rudhrapatel25 <rudhrapatel25@gmail.com>
2026-01-04 13:49:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("fade-in");
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+reveals.forEach(el => observer.observe(el));
